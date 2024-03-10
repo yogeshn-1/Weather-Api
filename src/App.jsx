2a6b6ccf039a6595c17e084106bf9900599cd2dd
yogeshn-1 <nirmalkary@gmail.com>
2024-03-10 16:02:21 +0530

    Layout and checking responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,11 @@ function App() {
     );
   };
   return (
-    <div className="flex flex-col m-2">
-      <div className="flex gap-4 w-full max-w-6xl m-2 mx-auto">
+    <div className="flex flex-col gap-2 m-2">
+      <h4 className="text-center text-3xl sm:text-2xl xs:text-xl font-serif mt-2">
+        Weather app
+      </h4>
+      <div className="flex gap-2 w-full max-w-6xl m-2 mx-auto">
         <input
           onChange={(e) => {
             setParam(e.target.value);
@@ -38,13 +41,13 @@ function App() {
         />
         <button
           onClick={searchCity}
-          className="px-2 py-1 mx-2 bg-orange-200 rounded-lg"
+          className="px-2 py-1 mx-2 bg-orange-200 hover:bg-orange-400 rounded-lg duration-200"
         >
           search
         </button>
       </div>
       {cityInfo && (
-        <div className="flex gap-2 items-center justify-center border bg-blue-200 w-full rounded-md my-2">
+        <div className="flex gap-2 items-center justify-center border bg-white w-fit rounded-md my-2 mx-auto">
           <div className="p-2">
             <p>
               {cityInfo.location.name}, {cityInfo.location.region}
@@ -53,18 +56,18 @@ function App() {
           </div>
           <p>{cityInfo.current.temp_c} &deg;C</p>
           <button
-            className="px-2 py-1 mx-2 bg-green-200 rounded-lg h-fit"
+            className="mx-2 px-2 py-1 bg-green-300 rounded-lg hover:bg-green-500 duration-200"
             onClick={() => addCity(cityInfo)}
           >
             Add
           </button>
         </div>
       )}
-      <section className="grid grid-cols-2 xs:flex xs:flex-col bg-gray-500 rounded-xl gap-2 ">
+      <section className="grid grid-cols-2 sm:flex sm:flex-col xs:flex xs:flex-col rounded-xl gap-2 font-mono">
         {cities.map((city) => (
           <div
             key={city.location.name}
-            className="flex gap-2 m-2 xs:mx-auto sm:text-sm xs:text-xs items-center border p-1 rounded-lg bg-white"
+            className="flex gap-2 m-2 xs:mx-auto sm:text-sm xs:text-sm items-center border p-1 rounded-lg bg-white"
           >
             <img src={city.current.condition.icon} alt="" />
             <div className="flex flex-col gap-0.5 flex-1">
@@ -77,7 +80,10 @@ function App() {
                 <p>Humidity: {city.current.humidity} %</p>
               </span>
             </div>
-            <button className="mx-2" onClick={() => removeCity(city)}>
+            <button
+              className="mx-2 px-2 py-1 rounded-md bg-red-300 hover:bg-red-500 duration-200"
+              onClick={() => removeCity(city)}
+            >
               Remove
             </button>
           </div>

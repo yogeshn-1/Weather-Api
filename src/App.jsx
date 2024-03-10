4649b57b47bb174edd83fa47accb7d707839db09
yogeshn-1 <nirmalkary@gmail.com>
2024-03-10 14:33:46 +0530

    Install tailwind and implement serach and add button city weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,62 @@
+import { useEffect, useState } from "react";
+import config from "./config";
+
 function App() {
+  const [city, setCity] = useState("");
+  const [cities, setCities] = useState([]);
+  const [cityInfo, setCityInfo] = useState(null);
+  const searchCity = () => {
+    if (city.length !== 0) {
+      //call api
+      fetch(`${config.baseURL}/current.json?key=${config.apiKey}&q=${city}`)
+        .then((res) => res.json())
+        .then((response) => {
+          if (response.error) throw new Error(response.error.message);
+          console.log(response);
+          setCityInfo(response);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+  const addCity = (city) => {
+    setCities((prev) => [...prev, city]);
+  };
+  useEffect(() => {
+    console.log("city", city);
+    console.log("cities", cities);
+  }, [city, cities]);
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <div className="flex flex-col m-2">
+      <div className="flex gap-4 w-full max-w-6xl m-2 mx-auto">
+        <input
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+          placeholder="Enter a city name"
+          className="flex-1 border rounded-md px-2 py-1"
+        />
+        <button
+          onClick={searchCity}
+          className="px-2 py-1 mx-2 bg-orange-200 rounded-lg"
+        >
+          search
+        </button>
+      </div>
+      {cityInfo && (
+        <div className="flex gap-2">
+          <p>
+            {cityInfo.location.name}, {cityInfo.location.region}
+          </p>
+          <button
+            className="px-2 py-1 mx-2 bg-green-200 rounded-lg"
+            onClick={() => addCity(cityInfo.location.name)}
+          >
+            Add
+          </button>
+        </div>
+      )}
+      <section className="grid grid-cols-2 xs:flex xs:flex-col bg-gray-500 rounded-xl p-4 gap-2"></section>
+    </div>
   );
 }
 
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  baseURL: "http://api.weatherapi.com/v1",
+  apiKey: "192e3aaf21ff4c0eaab95337240503",
+};
+
+export default config;
